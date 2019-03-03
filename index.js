a3d91b2e2782e9246e3a9fda08938ecde746d061
Youngsoo Han <amazingmobdev@gmail.com>
2019-03-04 02:06:50 +0900

    make utils for developing iPhone SE

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Dimensions, Platform, StatusBar } from "react-native";
+
+export const isIphoneSE = () => {
+  const { width, height } = Dimensions.get("window");
+
+  return (
+    Platform.OS === "ios" &&
+    !Platform.isPad &&
+    !Platform.isTVOS &&
+    (height === 320 || width === 320 || (height === 568 || width === 568))
+  );
+};
+
+export const concernSESize = (originalSize, seSize) => {
+  return isIphoneSE() ? originalSize : seSize;
+};
